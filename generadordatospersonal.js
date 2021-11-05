e20aaa78bab5298deacffaefc2099aa752c78342
LeixerSteevenLucasMero <79165244+LeixerSteevenLucasMero@users.noreply.github.com>
2021-11-05 12:10:26 -0500

    personal adminstrativo 2.0

--- generadordatospersonal.js
+++ generadordatospersonal.js
@@ -18,26 +18,41 @@ mongoose.connect(MONGO_URI, {
         Datos: "https://carreras.uleam.edu.ec/facci/wp-content/uploads/sites/26/2016/06/Hoja-de-Vida-Dolores-Mu%c2%a6oz.pdf",
       },
       personall: {
-        nombre: "Solorzano Cadena Ruben Dario", //decana
-        cedula:"13062971597",
+        nombre: "Solorzano Cadena Ruben Dario", //director carrera
+            cedula:"1306297159",
         facultad: "Facultad de Ciencias Informáticas", //facci
         Cargo: "Director de Carrera", //
         Datos: "https://carreras.uleam.edu.ec/facci/wp-content/uploads/sites/26/2019/02/Ing.-R%c3%baben-Solorzano.pdf",
       },
       personall: {
-        nombre: "Ayovi Ramirez Marco Wellington", //decana
+        nombre: "Ayovi Ramirez Marco Wellington", //docente
         cedula:"0800631152",
         facultad: "Facultad de Ciencias Informáticas", //facci
         Cargo: "Docente Universitario", //
         Datos: "https://carreras.uleam.edu.ec/facci/wp-content/uploads/sites/26/2019/02/Ing.-Marcos-Ayov%c3%ad.pdf",
       },
       personall: {
-        nombre: "Vera Navarrate Denise Soroya", //decana
+        nombre: "Arteaga Vera José Cristóbal", //docente 
+        cedula:"1304650854",
+        facultad: "Facultad de Ciencias Informáticas", //facci
+        Cargo: "Docente Universitario", //
+        Datos: "https://carreras.uleam.edu.ec/facci/wp-content/uploads/sites/26/2018/05/ING.-ARTEAGA-VERA-JOS%c3%89.pdf",
+
+      }, personall: {
+        nombre: "González López Oscar Armando", //docente
+        cedula:"1307613727",
+        facultad: "Facultad de Ciencias Informáticas", //facci
+        Cargo: "Docente Universitario", //
+        Datos: "https://carreras.uleam.edu.ec/facci/wp-content/uploads/sites/26/2018/05/A.S.-GONZALEZ-LOPEZ-OSCAR.pdf",
+      },
+      personall: {
+        nombre: "Vera Navarrate Denise Soroya", //secretaria
         cedula:"1308400249",
         facultad: "Facultad de Ciencias Informáticas", //facci
         Cargo: "Secretaria", //
         Datos: "https://carreras.uleam.edu.ec/facci/wp-content/uploads/sites/26/2018/05/ING.-VERA-NAVARRETE-DENISE.pdf",
       },
+
 
     },
   ];
